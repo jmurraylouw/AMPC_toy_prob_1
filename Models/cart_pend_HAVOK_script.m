a3fa464b51f683a4e3bdf5f8659d4370c80a258f
Murray <Murray@Luna>
2020-06-17 16:23:57 +0200

    HAVOK working with a little bit of noise

--- Models/cart_pend_HAVOK_script.m
+++ Models/cart_pend_HAVOK_script.m
@@ -7,6 +7,7 @@
 load('cartpend_data_3.mat') % Load training data
 u_data  = out.u.Data';
 x_data  = out.x.Data';
+plot(t,x_data)
 n = size(x_data)*[1; 0]; % number of states
 I = eye(n);
 y_data  = I([1, 3],:)*x_data; % Measure x and theta
@@ -16,6 +17,11 @@ l = size(u_data)*[1; 0]; % number of inputs
 t  = out.tout';
 Ts = t(2)-t(1);     % Sample time of data
 N  = length(t); % Number of data samples
+
+% Add noise
+sigma = 0.001;
+y_data = y_data + sigma*randn(size(y_data));
+plot(t,y_data)
 
 %%
 % Validation data
@@ -39,9 +45,10 @@ p = 12; % Truncated rank of system
 c = 2; % Column spacing of Hankel matrix
 d = 2; % Row spacing of Hankel matrix
 q = 800; % number of delays
-w = 1500; % (named 'p' in Multiscale paper) number of columns in Hankel matrix
+w = 1600; % (named 'p' in Multiscale paper) number of columns in Hankel matrix
 
 numel_H = q*m*w
+time_predict = 6e-6*numel_H
 
 final_sample = (q-1)*d + (w-1)*c + 2; % Last sample used in Hankel matrix
 assert(final_sample <= N, 'Not enough data. Change q, d, w, or c'); % otherwise index out of bounds 
@@ -55,8 +62,6 @@ tic;
 % Noticed error increased for increasing tau
 % i.e Y = y(k)      y(k+1)      y(k+2)...
 %         y(k+tau)  y(k+tau+1)  y(k+tau+2)...
-
-
 
 % Step 1: Collect and construct the snapshot matrices:
 % According to: Discovery of Nonlinear Multiscale Systems: Sampling Strategies and Embeddings
@@ -77,7 +82,9 @@ Omega = [X; Upsilon]; % Omega is concatination of Y and Upsilon
 % Step 2: Compute and truncate the SVD of the input space Omega
 disp(2)
 [U,S,V] = svd(Omega, 'econ');
-figure, semilogy(diag(S), 'x')
+figure, semilogy(diag(S), 'x'), hold on;
+title('Singular values of Omega, showing truncation')
+plot(p,S(p,p), 'ro'), hold off;
 U_tilde = U(:, 1:p); % Truncate SVD matrixes of Omega
 S_tilde = S(1:p, 1:p);
 V_tilde = V(:, 1:p);
@@ -87,7 +94,9 @@ U2_tilde = U_tilde(q*m+1:end, :);
 % Step 3: Compute the SVD of the output space X'
 disp(3)
 [U,S,V] = svd(X2, 'econ');
-figure, semilogy(diag(S), 'x')
+figure, semilogy(diag(S), 'x'), hold on;
+title('Singular values of X2, showing truncation')
+plot(r,S(r,r), 'ro'), hold off;
 U_hat = U(:, 1:r); % Truncate SVD matrixes of X2
 S_hat = S(1:r, 1:r);
 V_hat = V(:, 1:r);
@@ -109,9 +118,11 @@ if (sum(abs(eig(A)) > 1) ~= 0) % If some eigenvalues are unstable due to machine
     disp('Unstable eigenvalues')
 end
 
+% If some eigenvalues are unstable due to machine tolerance,
+% % Scale them to be stable
 % count = 0;
-% while (sum(abs(eig(A)) > 1) ~= 0) % If some eigenvalues are unstable due to machine tolerance
-%     count=count+1;
+% while (sum(abs(eig(A)) > 1) ~= 0) 
+%     count=count+1
 %     [Ve,De] = eig(A);
 %     unstable = abs(De)>1; % indexes of unstable eigenvalues
 %     De(unstable) = De(unstable)./abs(De(unstable)) - 10^(-10+count); % Normalize all unstable eigenvalues (set abs(eig) = 1)
@@ -149,40 +160,45 @@ toc;
 figure, plot(t_valid, x_valid([1, 3],:)) 
 title('Validation data vs Model')
 hold on;
+plot(t_valid, u_valid, ':', 'LineWidth', 1)
 plot(t_valid, x_hat, '--', 'LineWidth', 1); % Plot only non-delay coordinate 
+ylim([-4 7]) % Set equal ylim for comparison
+plot([D D], ylim, 'r')
+legend('x', 'theta', 'input', 'x_hat', 'theta_hat', 'D')
 hold off;
 
 %% Compare to training data
 disp(6)
 % Initial conditions
-x_hat_0 = zeros(q*m,1);
+x_hat_02 = zeros(q*m,1);
 for row = 0:q-1 % Create first column of spaced Hankel matrix
-    x_hat_0(row*m+1:(row+1)*m, 1) = y_data(:, row*d + 1);
+    x_hat_02(row*m+1:(row+1)*m, 1) = y_data(:, row*d + 1);
 end
 k_start = row*d + 1; % First k to start at
 
-X_hat = zeros(length(x_hat_0),N); % ??? Estimated X from model
-X_hat(:,k_start) = x_hat_0; % Initial conditions, insert at first k
+X_hat2 = zeros(length(x_hat_0),N); % ??? Estimated X from model
+X_hat2(:,k_start) = x_hat_02; % Initial conditions, insert at first k
 for k = (row*d + 1):N-1
-    X_hat(:,k+1) = A*X_hat(:,k) + B*u_data(k);
+    X_hat2(:,k+1) = A*X_hat2(:,k) + B*u_data(k);
 end
 
-x_hat = X_hat(end-m+1:end, :); % Extract only non-delay time series (last m rows)
+x_hat2 = X_hat2(end-m+1:end, :); % Extract only non-delay time series (last m rows)
 
 toc;
 
 figure, plot(t, y_data)
 title('Training data vs Model')
 hold on;
-plot(t, x_hat, '--', 'LineWidth', 1); % Plot only non-delay coordinate  
-
+plot(t, u_data, ':', 'LineWidth', 1)
+plot(t, x_hat2, '--', 'LineWidth', 1); % Plot only non-delay coordinate  
+ylim([-4 7]) % Set equal ylim for comparison
 plot([D D], ylim, 'r')
 plot([t(final_sample) t(final_sample)], ylim, 'k')
-legend('x', 'theta', 'D', 't(final_sample)')
+legend('x', 'theta', 'input', 'x_hat', 'theta_hat', 'D', 't(final sample)')
 hold off
 
-RMSE1 = mean(abs(x_valid(1,:).^2 - x_hat(1,:).^2))
-RMSE_matrix(q,r) = RMSE1;
+ave_diff = mean(abs((x_valid(1,k_start:end) - x_hat(1,k_start:end))))
+RMSE_matrix(q,r) = ave_diff;
 
 
     end
