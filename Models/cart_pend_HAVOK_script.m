5c5e6ad72b0073cbf57286fea692924b02eff071
Murray <Murray@Luna>
2020-07-11 00:06:54 +0200

    Working at adding results to saved data. Still jumping around quick a bit

--- Models/cart_pend_HAVOK_script.m
+++ Models/cart_pend_HAVOK_script.m
@@ -14,6 +14,12 @@ u_data  = out.u.Data';
 x_data  = out.x.Data';
 y_data  = x_data([1,3],:); % Measurement data (x and theta)
 
+% Testing data - Last 50 s is for testing and one sample overlaps training 
+y_test = y_data(:,end-N_test+1:end); % One sample of testing data overlaps for initial condition
+u_test = u_data(:,end-N_test+1:end);
+t_test = t(:,end-N_test+1:end);
+
+% Data dimentions
 n = size(x_data,1); % number of states
 m = size(y_data,1); % number of measurements
 l = size(u_data,1); % number of inputs
@@ -33,25 +39,26 @@ c = 1; % Column spacing of Hankel matrix (for multiscale dynamics)
 d = 1; % Row spacing of Hankel matrix (for multiscale dynamics)
 % N_train; % Num of data samples for training, rest for testing
 % w; % (named 'p' in Multiscale paper) number of columns in Hankel matrix
-N_train_min = 10/Ts; % Minimum length of training data
-N_train_max = 30/Ts; % Maximum length of training data
-% max_iterations = 1000; % Maximum number of iterations allowed in Random search
-% p = 34; % Truncated rank of system
-p_min = 28; % Min value of p in Random search
-p_max = 35; % Max value of p in Random search
-% q = 1000; % number of delays
-q_min = 80; % Min value of q in Random search
-% q_max; % Max value of q in Random search
-q_increment = 50; % Increment value of q in Grid search
+N_train_min = 10.5/Ts; % Minimum length of training data
+N_train_max = 49.5/Ts; % Maximum length of training data
+% p = Truncated rank of system of Omega
+% r = Truncated rank of system of X2
+% q = number of delays
+p_min = 4; % Min value of p in Random search
+p_max = 200; % Max value of p in Random search
+q_min = 4; % Min value of q in Random search
+q_max = 500; % Max value of q in Random search
+q_increment = 10; % Increment value of q in Grid search
 p_increment = 2; % Increment value of p in Grid search
+N_train_increment = 100; % Increment value of N_train in Grid search
 
-p_search = p_min:p_increment:p_max; % List of p to search, for every q
+q_search = q_min:q_increment:q_max; % List of q parameters to search in
 
 % Add noise once
 y_data_noise = y_data + sigma*randn(size(y_data));
 
 % Lists to save results for different N_train
-N_train_list = N_train_min:1000:N_train_max; % List of N_train to compute model for
+N_train_list = N_train_min:N_train_increment:N_train_max; % List of N_train to compute model for
 MAE_list = Inf + zeros(m,length(N_train_list)); % Save best MAE for each N_train
 p_list = -1 + zeros(1,length(N_train_list)); % Save p of best MAE for each N_train
 q_list = -1 + zeros(1,length(N_train_list)); % Save q of best MAE for each N_train
@@ -60,55 +67,44 @@ time_list = -1 + zeros(1,length(N_train_list)); % Save time taken for each N_tra
 % Number of iterations predicted
 num_iterations = (p_max - p_min)/p_increment*((N_train_max+N_train_min)/4 - q_min)/q_increment*length(N_train_list)
 
+try
+    load('Data\N_train_error_time_HAVOK_sig=0.mat');
+catch
+    disp('No saved results to compare to')  
+    N_train_saved = [];
+end
+
 %% Loop through different training lengths
 for index = 1:length(N_train_list) % Loop through N_train_list
     % index is used to relate all the lists to N_train_list
-    N_train = N_train_list(index); 
-  
-    % Testing data - Last 50 s is for testing and one sample overlaps training 
-    y_test = y_data(:,end-N_test+1:end); % One sample of testing data overlaps for initial condition
-    u_test = u_data(:,end-N_test+1:end);
-    t_test = t(:,end-N_test+1:end);
+    disp('--------------------------')
+    N_train = N_train_list(index)
+    disp('searching...')
 
     % Training data - Last sample of training is first sample of testing
     y_train = y_data_noise(:,end-N_test-N_train+2:end-N_test+1); % Use noisy data
     u_train = u_data(:,end-N_test-N_train+2:end-N_test+1);
     t_train = t(:,end-N_test-N_train+2:end-N_test+1);
 
-    try
-        load('Data\N_train_error_time_HAVOK_sig=0.mat');
-        N_train_saved
-        if ismember(N_train,N_train_saved)
-            % Use previously saved best results
-            save_index = find(N_train_saved == N_train); % Index of N_train in saved list
-            MAE_best = MAE_saved(:,save_index); % 
-            p_best = p_saved(save_index);
-            q_best = q_saved(save_index);
-            time_best = time_saved(save_index);   
-            
-        else % If no saved data about this N_train:
-            save_index = -1; % -1 means no saved data exits for N_train
-            MAE_best = Inf*[1;1];
-            p_best = NaN;
-            q_best = NaN;
-            time_best = NaN;
-        end
-        
-    catch
-        disp('No saved results to compare to')    
-        save_index = -2; % -1 means no saved data exits for N_train
+    if ismember(N_train,N_train_saved)
+        % Use previously saved best results
+        save_index = find(N_train_saved == N_train); % Index of N_train in saved list
+        MAE_best = MAE_saved(:,save_index); % 
+        p_best = p_saved(save_index);
+        q_best = q_saved(save_index);
+        time_best = time_saved(save_index);   
+
+    else % If no saved data about this N_train
+        save_index = -1; % -1 means no saved data exits for N_train
+        MAE_best = Inf*[1;1];
         p_best = NaN;
         q_best = NaN;
-        MAE_best = Inf*[1;1];
         time_best = NaN;
     end
-    
-    save_index
 
-    q_max = floor(N_train/3); % Max q when Hankel is a square
+%     q_max = floor(N_train/3); % Max q when Hankel is a square
     
     % Grid search for best params: p,q for each N_train
-    q_search = q_min:q_increment:q_max; % List of q parameters to search in
     for q = q_search
 
 %     %  Random search for best hyperparameters
@@ -156,6 +152,7 @@ for index = 1:length(N_train_list) % Loop through N_train_list
         time_q = toc(timer_q); % record time for first part in q loop
         
         % Grid search: Search through these p values for current q    
+        p_search = p_min:p_increment:min([p_max,q*m]); % List of p to search, for every q
         for p = p_search
             
             timer_p = tic; % start timer of part in p loop
@@ -242,12 +239,12 @@ for index = 1:length(N_train_list) % Loop through N_train_list
             MAE = sum(abs(y_hat - y_test), 2)./N_test; % For each measured state
 
             if mean(MAE) < mean(MAE_best)
-                MAE_best = MAE;
-                p_best = p;
-                q_best = q;
-                time_best = time;
+                MAE_best = MAE
+                p_best = p
+                q_best = q
+                time_best = time
                 
-                if save_index == -2 % If no saved data file exists
+                if isempty(N_train_saved) % If no saved data file existed
                     N_train_saved = [N_train];
                     MAE_saved = [MAE_best];
                     p_saved = [p_best];
@@ -271,6 +268,7 @@ for index = 1:length(N_train_list) % Loop through N_train_list
                                 break; 
                             end
                         end
+                        
                     else % If N_train is the biggest yet, add to end of list
                         N_train_saved = [N_train_saved, N_train];
                         MAE_saved = [MAE_saved, MAE_best];
@@ -293,7 +291,6 @@ for index = 1:length(N_train_list) % Loop through N_train_list
                 
                 % Save results
                 save('Data\N_train_error_time_HAVOK_sig=0.mat', 'N_train_saved', 'MAE_saved', 'p_saved', 'q_saved', 'time_saved');
-
             end
 
             % %% Compare to training data
@@ -339,17 +336,18 @@ for index = 1:length(N_train_list) % Loop through N_train_list
     q_list(:,index) = q_best;
     time_list(:,index) = time_best; % Save time taken to compute model for each N_train
       
-    disp('--------------------------')
-    N_train
+    disp('Results:')
     MAE_best
     p_best
     q_best
     time
+    disp('--------------------------')
     
 end
 
 
 %%
+disp('Plotting...')
 figure(1), plot(N_train_saved,MAE_saved(1,:)), title('MAE x vs N-train')
 figure(2), plot(N_train_saved,MAE_saved(2,:)), title('MAE theta vs N-train')
 figure(3), plot(N_train_saved,p_saved), title('p vs N-train')
